3b6d39daad61f860bb0002f30e4bda02e57b95eb
Alejandro Rodríguez Arevalillo <148347190+Alex13042001@users.noreply.github.com>
2025-09-05 13:08:12 +0200

    Código base del script.py

--- monitor.py
+++ monitor.py
@@ -1,1 +1,11 @@
+import os
 
+# Lista de hosts a comprobar
+hosts = ["8.8.8.8", "1.1.1.1", "www.google.com", "www.github.com"]
+
+for host in hosts:
+    respuesta = os.system(f"ping -c 1 {host}")  # en Windows usa: ping -n 1
+    if respuesta == 0:
+        print(f"{host} está disponible ✅")
+    else:
+        print(f"{host} no responde ❌")
